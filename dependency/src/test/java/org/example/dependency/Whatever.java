4888d54aa04103bebe442f35ff8500c8ed0e2e50
Kirill <kirill.gagarski@gmail.com>
2023-01-31 19:33:36 +0300

    Initial commit

---
+++ dependency/src/test/java/org/example/dependency/Whatever.java
@@ -0,0 +1,9 @@
+package org.example.dependency;
+
+public class Whatever {
+    private Whatever() {}
+
+    public static void doSmth() {
+        System.out.println("Hello!");
+    }
+}
---
+++ main/src/main/java/org/example/main/Main.java
@@ -0,0 +1,9 @@
+package org.example.main;
+
+import org.example.dependency.Whatever;
+
+public class Main {
+    public static void main(String[] args) {
+        Whatever.doSmth();
+    }
+}
